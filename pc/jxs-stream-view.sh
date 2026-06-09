6aaf2c39aabc9b2c4d0c0d0e71347b438d42577e
dgperkins <dgperkins@gmail.com>
2026-06-08 21:08:54 -0400

    feat: JPEG-XS viewer joins island multicast group on eth1

--- pc/jxs-stream-view.sh
+++ pc/jxs-stream-view.sh
@@ -10,7 +10,7 @@ source "$DIR/jxs-stream-env.sh"
 echo "JPEG-XS viewer <- udp://$JXS_ADDR:$JXS_PORT  (GPU window; close window or Ctrl+C to stop)"
 ffmpeg -hide_banner -loglevel warning \
     -fflags nobuffer -flags low_delay \
-    -i "udp://${JXS_ADDR}:${JXS_PORT}?overrun_nonfatal=1&fifo_size=5000000&buffer_size=67108864" \
+    -i "udp://${JXS_ADDR}:${JXS_PORT}?localaddr=${JXS_LOCALADDR}&overrun_nonfatal=1&fifo_size=5000000&buffer_size=67108864" \
     -f rawvideo -pix_fmt "${JXS_PIXFMT}" - \
   | gst-launch-1.0 -q \
       fdsrc ! rawvideoparse format="${JXS_GSTFMT}" width="${JXS_W}" height="${JXS_H}" framerate="${JXS_FPS}" \
